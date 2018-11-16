10d71ba4cdf745c85797c4e5e97827ddc14756be
hj_yang <hj_yang@Ctrip.com>
2018-11-16 18:34:05 +0800

    add file

---
+++ ssj/src/main/java/com/hans/yang/web/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.hans.yang.web.controller;
+
+import com.hans.yang.bean.User;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created In Ctrip. <br/>
+ *
+ * @author: hj_yang <br/>
+ * @date: 2018/11/2 <br/>
+ */
+@RestController
+@RequestMapping(value="/")
+public class HomeController {
+
+  @RequestMapping(value = "/index",method = RequestMethod.GET)
+  public User getUser(){
+
+    User userBean = new User();
+    userBean.setId(121);
+    userBean.setName("hans");
+    return userBean;
+  }
+
+
+
+}
