07d03671a48b9118301017043a9a7c134e0a3773
hanjingyang <115877264@qq.com>
2019-03-28 11:19:58 +0800

    jenkins

--- ssj/src/main/java/com/hans/yang/Application.java
+++ ssj/src/main/java/com/hans/yang/Application.java
@@ -23,14 +23,14 @@ import org.springframework.context.annotation.Configuration;
 */
 @EnableAutoConfiguration
 @ComponentScan
-/* @SpringbootApplication
+/* @SpringbootApplication EmbeddedServletContainerCustomizer
 注解  可以解决根类或者配置类（我自己的说法，就是main所在类）头上注解过多
 的问题，一个@SpringbootApplication相当于@Configuration,@EnableAutoConfiguration和 @ComponentScan
 并具有他们的默认属性值 */
 /*如果是要打成war包，需要继承SpringBootServletInitializer 并且重写configure方法*/
 public class Application extends SpringBootServletInitializer {
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(Application.class,  args);
   }
 
   @Override
